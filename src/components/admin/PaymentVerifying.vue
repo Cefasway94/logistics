ce8f7f533e4580a9e6dc895ce589cf9dbca48980
Sudi <kekovasudi@gmail.com>
2020-05-05 18:51:16 +0300

    adding payment verifying page

---
+++ src/components/admin/PaymentVerifying.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+
+      <v-card class="mx-auto mt-12" color="#F5FAFF">
+
+          <AlertError v-if="display_alert" v-bind:message="alert"/>
+
+          
+
+      </v-card>
+     
+  </v-container>
+</template>
+
+<script>
+//import axios from 'axios'
+import AlertError from '@/components/AlertError.vue'
+
+export default {
+  
+  name: 'PaymentVerifying',
+
+  data (){
+
+    return{
+
+      dialog: false,
+
+      alert:'',
+      display_alert: false,
+
+      email:''
+
+    }
+  },
+
+  components: {AlertError},
+
+  methods: {
+    
+
+
+  },
+
+  
+
+  beforeRouteEnter(to, from, next){
+    next(vm => {
+        
+    
+      next();
+    })
+
+  }
+}
+</script>
+
+<style>
+
+</style>
